3e1496925af10f392eba3fa85df98be1fd40fdce
Aaron Block <ablock@austincollege.edu>
2019-02-08 14:34:48 -0600

    Initial Checkin

--- SisGame/GameViewController.swift
+++ SisGame/GameViewController.swift
@@ -1,9 +1,9 @@
 //
 //  GameViewController.swift
-//  SisGame
+//  SampleSpriteKit
 //
-//  Created by Aaron Block on 2/8/19.
-//  Copyright © 2019 ACCrew. All rights reserved.
+//  Created by Aaron Block on 2/4/19.
+//  Copyright © 2019 Aaron Block. All rights reserved.
 //
 
 import UIKit
@@ -11,41 +11,27 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let view = self.view as! SKView? {
+            let scene = SampleScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            scene.backgroundColor = UIColor.black
+            view.presentScene(scene)
             
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -53,7 +39,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
---
+++ SisGame/SampleScene.swift
@@ -0,0 +1,129 @@
+//
+//  SampleScene.swift
+//  SampleSpriteKit
+//
+//  Created by Aaron Block on 2/4/19.
+//  Copyright © 2019 Aaron Block. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+
+/* Todo */
+// - 1. Set up basic scene
+// - 2. Add ball
+// 3. Move ball
+// 4. bounce ball on edge
+// 5. bounce ball on object
+// 7. Bounce ball add gravity
+// 8. Detect Touch
+// 9. Detect drag
+// 10. increase accelleration to ball with drag
+// 11. Increase acceleration in direction with drag.
+
+//SKScenes are the "view" equivalant for sprite kit.
+class SampleScene: SKScene {
+    private var mainNode:SKNode?
+
+    // Shape node might be approriate for ball and maybe approriate for other shapes, but
+    // too many can impact performance
+    private var ball : SKShapeNode?
+    private var ball2 : SKShapeNode?
+    private var chargeValue:CGFloat!
+    private var startPoint:CGPoint?
+    
+    //didMove is the method that is called when the system is loaded.
+    override func didMove(to view: SKView) {
+        
+        mainNode = SKNode()
+        chargeValue = 0.0
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.05
+        var hexagonPoints = [CGPoint(x: 0, y: -20),
+                             CGPoint(x: -19, y: -6),
+                             CGPoint(x: -12, y: 16),
+                             CGPoint(x: 12, y: 16),
+                             CGPoint(x: 19, y: -6),
+                             CGPoint(x: 0, y: -20)]
+        
+        //self.ball = SKShapeNode(ellipseOf: CGSize(width: w, height: w))
+        self.ball = SKShapeNode(points: &hexagonPoints, count: 6)
+        self.ball?.position = CGPoint(x: 320, y: 320)
+        self.ball?.fillColor = UIColor.red
+        //self.ball?.physicsBody = SKPhysicsBody(circleOfRadius: w/2)
+        self.ball?.physicsBody = SKPhysicsBody(polygonFrom: self.ball!.path!)
+        self.ball?.physicsBody?.usesPreciseCollisionDetection = true
+        self.ball?.physicsBody?.friction = 1.0
+
+        
+        self.ball2 = SKShapeNode(ellipseOf: CGSize(width: w, height: w))
+        self.ball2?.position = CGPoint(x: 200, y: 320)
+        self.ball2?.fillColor = UIColor.blue
+        self.ball2?.physicsBody = SKPhysicsBody(circleOfRadius: w/2)
+        self.ball2?.physicsBody?.usesPreciseCollisionDetection = true
+        self.ball2?.physicsBody?.friction = 1.0
+
+        
+        // Create the ground node and physics body
+        var splinePoints = [CGPoint(x: 0, y: 500),
+                            CGPoint(x: 100, y: 50),
+                            CGPoint(x: 400, y: 110),
+                            CGPoint(x: 640, y: 20)]
+        
+        let ground = SKShapeNode(splinePoints: &splinePoints,
+                                 count: splinePoints.count)
+        ground.lineWidth = 5
+        ground.physicsBody = SKPhysicsBody(edgeChainFrom: ground.path!)
+        ground.physicsBody?.restitution = 1.0
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.friction = 1.0
+        
+        // Add the two nodes to the scene
+        mainNode?.addChild(self.ball!)
+        mainNode?.addChild(self.ball2!)
+        mainNode?.addChild(ground)
+        
+        self.addChild(mainNode!)
+        //self.addChild(self.ball!)
+        
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            print("Touches started")
+            startPoint = touch.location(in: self)
+            print("x:\(touch.location(in: self).x),y:\(touch.location(in: self).y) ")
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            print("Touches moved")
+            print("x:\(touch.location(in: self).x),y:\(touch.location(in: self).y) ")
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first, let startPoint = self.startPoint{
+            print("Touches ENDED")
+            
+            let endPoint = touch.location(in: self)
+            print("x:\(touch.location(in: self).x),y:\(touch.location(in: self).y) ")
+
+            let factor : CGFloat = 1.0
+            let charge = CGVector(dx: factor*(startPoint.x - endPoint.x), dy: factor*(startPoint.y - endPoint.y))
+            ball?.physicsBody?.applyImpulse(charge)
+        }
+        
+    }
+    
+//    override func didFinishUpdate() {
+//        let xPos = self.size.width/2.0 - ball!.position.x
+//        let yPos = self.size.height/2.0 - ball!.position.y
+//        mainNode?.position = CGPoint(x:xPos,y:yPos)
+//    }
+    
+}
